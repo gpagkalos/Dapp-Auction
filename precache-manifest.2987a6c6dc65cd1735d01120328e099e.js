a0b342f11dbf436d6cb960f9f9351b8a9f868d9e
gpagkalos <g.pagkalos@hotmail.com>
2021-07-31 11:16:26 +0300

    Updates

---
+++ precache-manifest.2987a6c6dc65cd1735d01120328e099e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e028593434ff3a880f6f23154afae3c9",
+    "url": "/Dapp-Auction/index.html"
+  },
+  {
+    "revision": "b6f93d92e7c69685094d",
+    "url": "/Dapp-Auction/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "ec341ce6abf8ce0f48fa",
+    "url": "/Dapp-Auction/static/js/2.8c23eab5.chunk.js"
+  },
+  {
+    "revision": "b6f93d92e7c69685094d",
+    "url": "/Dapp-Auction/static/js/main.87598ad8.chunk.js"
+  },
+  {
+    "revision": "525eaa1706fd920d6246",
+    "url": "/Dapp-Auction/static/js/runtime-main.bdd05168.js"
+  }
+]);
